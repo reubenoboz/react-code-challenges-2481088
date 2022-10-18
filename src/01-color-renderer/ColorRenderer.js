1af6eeca10ac32a8bd2a3d8811a52c0717a4c18a
reubenoboz <reubenoboz@hotmail.com>
2022-10-18 13:31:03 +0100

    done

--- src/01-color-renderer/ColorRenderer.js
+++ src/01-color-renderer/ColorRenderer.js
@@ -19,9 +19,8 @@ const colors = [{
 
 export default function ColorRenderer () {
   return (
-    <h2>
-      Use the Color component to render each
-       item in the colors array on the page!
-    </h2>
+    <div>
+      {colors.map((item, index) => <Color key={index} hex={item.hex} name={item.name} />)}
+    </div>
   )
 }
